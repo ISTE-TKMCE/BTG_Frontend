ed9b34f89aa9d613acdaf8a29e6718653750a5f0
srk-sahad <78034275+srk-sahad@users.noreply.github.com>
2022-05-15 21:21:54 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from 'react';
+import './App.css';
+import TechnicalImage from "E:\\ISTE\\btg\\btgx\\src\\ab.jpg";
 const About = () => {
   return (
     <div>About</div>
   )
 }
 
+const divStyle = {
+  
+  backgroundImage: `url(${TechnicalImage})`,
+  backgroundSize: 'cover'  
+};
+
+
+function App() {
+  return (
+    <div style={{
+      backgroundImage: `url("E:\\ISTE\btg\btgx\\src\\ab.jpg")` 
+    }}>
+
+      <header className="App-header">
+        <p>
+          njcnfsv shv v sbvhnv vhnvjbvdcjsvndc dcvgdscbdvcgd
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
 export default About
